9c8a3b08d5fdd6540a1b898e0148c177c5743948
Violet334 <148483747+Violet334@users.noreply.github.com>
2025-03-27 19:36:57 -0400

    week 10

--- BehaviourTree/Assets/Scripts/GroundChild.cs
+++ BehaviourTree/Assets/Scripts/GroundChild.cs
@@ -1,6 +1,7 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
 using UnityEngine;
+using TMPro;
 
 
 namespace NodeCanvas.Tasks.Actions {
@@ -9,6 +10,14 @@ namespace NodeCanvas.Tasks.Actions {
 		public BBParameter<Transform> childPos;
 		public Transform groundedPos;
 
+		//create a delay timer
+		public float timer = 2;
+
+		public Renderer rend;
+		public Material defaultMaterial;
+		public Material material;
+
+		public TextMeshPro yelling;
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
@@ -19,12 +28,20 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			childPos.value.position = groundedPos.position;
-			EndAction(true);
+			rend.material = material;
+			yelling.text = "YOU ARE GROUNDED!!";
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
+			timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+				childPos.value.position = groundedPos.position;
+				yelling.text = "";
+				rend.material = defaultMaterial;
+				EndAction(true);
+			}
 			
 		}
 
