606c63cb364e947c4d209c0e4b1d49eaa22a273f
livio <livio@OA-S145-01>
2025-04-09 20:11:06 -0400

    wed3

--- BehaviourTree/Assets/Scripts/Player Script/PlayerController.cs
+++ BehaviourTree/Assets/Scripts/Player Script/PlayerController.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
+    private NavMeshAgent navAgent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        navAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
